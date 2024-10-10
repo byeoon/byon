84676c979d313dd12d77b0b3c9807323080a1e8b
Specifix5 <62927726+Specifix5@users.noreply.github.com>
2024-10-10 22:56:40 +0800

    more stuff

--- src/commands/komiJumpscare.ts
+++ src/commands/komiJumpscare.ts
@@ -1,5 +1,6 @@
 import { ApplicationIntegrationType, Client, CommandInteraction, InteractionContextType } from "discord.js"
 import { Command, UniversalContextType, UniversalIntegrationType } from "../commons/command"
+import { getConfigValue } from "../events/errorHandler"
 
 export const komiJumpscare: Command = {
     name: "jumpscare_me_onegaishimasu",
@@ -8,7 +9,7 @@ export const komiJumpscare: Command = {
     contexts: UniversalContextType,
     run: async (client: Client, interaction: CommandInteraction) => {
         await interaction.reply({
-            content: "https://cdn.discordapp.com/attachments/1147843031077228564/1293572523514003506/mikushow.png?ex=6707dccb&is=67068b4b&hm=8cbeaa872899a3a14b41941c1e317d8b5aeefe6c1495d3b4ca0b9c6feaf4d7cc&"
+            content: getConfigValue("JUMPSCARE_GIFS")[Math.floor(Math.random() * getConfigValue("JUMPSCARE_GIFS").length)]
         })
     }
 }
